902c06c0386241abb58f83375c4286df39939ab7
Zach Warner <jono.z.w@gmail.com>
2023-01-03 08:54:02 +0000

    WIP controller update

--- src/controllers.hpp
+++ src/controllers.hpp
@@ -2,7 +2,11 @@
 #define __CONTROLLERS_HPP__
 
 Controller::Controller();
+Controller::Controller(ControllerType cType);
 Controller::GetState();
 Controller::Update(void);
+
+Controller::AttachDevice();
+Controller::SetDeviceType(ControllerType cType);
 
 #endif // __CONTROLLERS_HPP__
